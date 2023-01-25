5322735c14a10607b553f01bf1ea4190d7bce802
ShuVeriDa <bashtarov@outlook.com>
2023-01-25 19:36:08 +0300

    refactored code by creating a single component for subscribe and unsubscribe from the community

--- src/community/community.service.ts
+++ src/community/community.service.ts
@@ -8,6 +8,7 @@ import { CommunityEntity } from './entity/community.entity';
 import { Repository } from 'typeorm';
 import { CreateCommunityDto } from './dto/create.dto';
 import { UserEntity } from '../user/entity/user.entity';
+import { subscribeAndUnSubscribe } from '../components/forServices/subscribeAndUnSubscribe';
 
 @Injectable()
 export class CommunityService {
@@ -96,81 +97,102 @@ export class CommunityService {
   }
 
   async subscribe(communityId: string, userId: string) {
-    const community = await this.communityRepository.findOne({
-      where: { id: communityId },
-      relations: ['members'],
-    });
-
-    if (!community) {
-      throw new NotFoundException(`Community with id ${communityId} not found`);
-    }
-
-    const isMember = community.members.find(
-      (c) => String(c.id) === String(userId),
+    return await subscribeAndUnSubscribe(
+      communityId,
+      userId,
+      this.communityRepository,
+      this.userRepository,
+      'subscribe',
     );
-    if (isMember) {
-      throw new ForbiddenException(
-        'This user already exists in this community',
-      );
-    }
 
-    await this.communityRepository.save({
-      ...community,
-      members: [...community.members, { id: userId }],
-    });
-
-    const existCommunity = await this.communityRepository.findOne({
-      where: { id: communityId },
-      relations: ['members'],
-    });
-
-    existCommunity.members.map((m) => {
-      delete m.password;
-      return m;
-    });
-
-    return existCommunity;
+    // const community = await this.communityRepository.findOne({
+    //   where: { id: communityId },
+    //   relations: ['members'],
+    // });
+    //
+    // if (!community) {
+    //   throw new NotFoundException(`Community not found`);
+    // }
+    //
+    // const user = await this.userRepository.findOneBy({ id: userId });
+    // if (!user) {
+    //   throw new NotFoundException(`User with id ${userId} not found`);
+    // }
+    //
+    // const isMember = community.members.find(
+    //   (c) => String(c.id) === String(userId),
+    // );
+    // if (isMember) {
+    //   throw new ForbiddenException(
+    //     'This user already exists in this community',
+    //   );
+    // }
+    //
+    // await this.communityRepository.save({
+    //   ...community,
+    //   members: [...community.members, { id: user.id }],
+    // });
+    //
+    // const existCommunity = await this.communityRepository.findOne({
+    //   where: { id: communityId },
+    //   relations: ['members'],
+    // });
+    //
+    // existCommunity.members.map((m) => {
+    //   delete m.password;
+    //   return m;
+    // });
+    //
+    // return existCommunity;
   }
 
   async unsubscribe(communityId: string, userId: string) {
-    const community = await this.communityRepository.findOne({
-      where: { id: communityId },
-      relations: ['members'],
-    });
-
-    if (!community) {
-      throw new NotFoundException(`Community with id ${communityId} not found`);
-    }
-
-    const user = await this.userRepository.findOneBy({ id: userId });
-    if (!user) {
-      throw new NotFoundException(`User with id ${userId} not found`);
-    }
-
-    const isMember = community.members.find(
-      (c) => String(c.id) === String(userId),
+    return await subscribeAndUnSubscribe(
+      communityId,
+      userId,
+      this.communityRepository,
+      this.userRepository,
+      'unsubscribe',
     );
-    if (!isMember) {
-      throw new ForbiddenException(
-        'This user does not exist in this community',
-      );
-    }
 
-    community.members = community.members.filter(
-      (member) => member.id !== user.id,
-    );
-    await this.communityRepository.save(community);
-
-    const existCommunity = await this.communityRepository.findOne({
-      where: { id: communityId },
-      relations: ['members'],
-    });
-
-    existCommunity.members.map((m) => {
-      delete m.password;
-      return m;
-    });
-
-    return existCommunity;
+    //   const community = await this.communityRepository.findOne({
+    //     where: { id: communityId },
+    //     relations: ['members'],
+    //   });
+    //
+    //   if (!community) {
+    //     throw new NotFoundException(`Community with id ${communityId} not found`);
+    //   }
+    //
+    //   const user = await this.userRepository.findOneBy({ id: userId });
+    //   if (!user) {
+    //     throw new NotFoundException(`User with id ${userId} not found`);
+    //   }
+    //
+    //   const isMember = community.members.find(
+    //     (c) => String(c.id) === String(userId),
+    //   );
+    //   if (!isMember) {
+    //     throw new ForbiddenException(
+    //       'This user does not exist in this community',
+    //     );
+    //   }
+    //
+    //   community.members = community.members.filter(
+    //     (member) => member.id !== user.id,
+    //   );
+    //   await this.communityRepository.save(community);
+    //
+    //   const existCommunity = await this.communityRepository.findOne({
+    //     where: { id: communityId },
+    //     relations: ['members'],
+    //   });
+    //
+    //   existCommunity.members.map((m) => {
+    //     delete m.password;
+    //     return m;
+    //   });
+    //
+    //   return existCommunity;
   }
 }
---
+++ src/components/forServices/subscribeAndUnSubscribe.ts
@@ -0,0 +1,67 @@
+import { Repository } from 'typeorm';
+import { CommunityEntity } from '../../community/entity/community.entity';
+import { UserEntity } from '../../user/entity/user.entity';
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
+
+export const subscribeAndUnSubscribe = async (
+  communityId: string,
+  userId: string,
+  communityRepos: Repository<CommunityEntity>,
+  userRepos: Repository<UserEntity>,
+  flag: 'subscribe' | 'unsubscribe',
+) => {
+  const community = await communityRepos.findOne({
+    where: { id: communityId },
+    relations: ['members'],
+  });
+
+  if (!community) {
+    throw new NotFoundException(`Community with id ${communityId} not found`);
+  }
+
+  const user = await userRepos.findOneBy({ id: userId });
+  if (!user) {
+    throw new NotFoundException(`User with id ${userId} not found`);
+  }
+
+  const isMember = community.members.find(
+    (c) => String(c.id) === String(userId),
+  );
+
+  if (flag === 'subscribe') {
+    if (isMember) {
+      throw new ForbiddenException(
+        'This user already exists in this community',
+      );
+    }
+
+    await communityRepos.save({
+      ...community,
+      members: [...community.members, { id: user.id }],
+    });
+  }
+
+  if (flag === 'unsubscribe') {
+    if (!isMember) {
+      throw new ForbiddenException(
+        'This user does not exist in this community',
+      );
+    }
+    community.members = community.members.filter(
+      (member) => member.id !== user.id,
+    );
+    await communityRepos.save(community);
+  }
+
+  const existCommunity = await communityRepos.findOne({
+    where: { id: communityId },
+    relations: ['members'],
+  });
+
+  existCommunity.members.map((m) => {
+    delete m.password;
+    return m;
+  });
+
+  return existCommunity;
+};
