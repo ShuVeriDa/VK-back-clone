6a1d3d04a423c183c28ffdd5c32870252707e534
ShuVeriDa <bashtarov@outlook.com>
2023-02-17 11:08:47 +0300

    added the ability to get all video

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { VideoService } from './video.service';
 
 @Controller('video')
-export class VideoController {}
+export class VideoController {
+  constructor(private readonly videoService: VideoService) {}
+
+  @Get('all')
+  getAll() {
+    return this.videoService.getAll();
+  }
+}
--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { VideoEntity } from './entity/video.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class VideoService {}
+export class VideoService {
+  @InjectRepository(VideoEntity)
+  private readonly videoRepository: Repository<VideoEntity>;
+
+  async getAll() {
+    const video = await this.videoRepository.find({
+      relations: ['communities'],
+      order: { createdAt: 'DESC' },
+    });
+
+    return video;
+  }
+}
