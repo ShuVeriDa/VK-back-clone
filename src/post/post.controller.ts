4fe82847bbddbf5697bd28d56f44ba82148ea501
ShuVeriDa <bashtarov@outlook.com>
2023-04-13 00:29:00 +0300

    refactored code

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -101,17 +101,17 @@ export class PostController {
   }
 
   //FOR COMMUNITY
-
-  @Get('/community/post')
-  getAllPostsInCommunity(@Body() dto: FetchPostDto) {
-    return this.postService.getAllPostsInCommunity(dto);
-  }
   @Get('/community/post/:id')
   getOnePostInCommunity(
     @Body() dto: FetchPostDto,
     @Param('id') postId: string,
   ) {
     return this.postService.getOnePostInCommunity(dto, postId);
+  }
+
+  @Post('/community/post/fetch')
+  getAllPostsInCommunity(@Body() dto: FetchPostDto) {
+    return this.postService.getAllPostsInCommunity(dto);
   }
 
   @Post('community/post/')
